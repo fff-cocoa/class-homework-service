b736f0dd6d86a32fe1d056df13a99fd8420858f6
fff-cocoa <incotnigo@outlook.com>
2022-05-28 11:33:06 +0800

    11:33

--- pages/home/index.js
+++ pages/home/index.js
@@ -24,9 +24,22 @@ Page({
     })
   },
   scrollToTop() {
-    wx.pageScrollTo({
-      scrollTop: 20,
-      duration: 300
+    console.log("233")
+    wx.navigateTo({
+      url: '/pages/submit/index',//附带两个参数过去
+      events: {
+        // 为指定事件添加一个监听器，获取被打开页面传送到当前页面的数据
+        acceptDataFromOpenedPage: function(data) {
+          console.log(data)
+        },
+        someEvent: function(data) {
+          console.log(data)
+        }
+      },
+      success: function(res) {
+        // 通过eventChannel向被打开页面传送数据
+        res.eventChannel.emit('acceptDataFromOpenerPage', { data: 'test' })
+      }
     })
   },
   onLoad() {
